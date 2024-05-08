3722e0fba5702f08c62fa02aa6ed904d1b6e6cc3
arthurborges <arthurborges.senai564@gmail.com>
2024-05-08 15:40:05 -0300

    feat: Add getBattlesByVillainName endpoint
    The battlesController and battlesRoutes files have been modified to add a new endpoint for retrieving battles by villain name. This allows users to get battles where a specific villain is either the winner or the loser. This change enhances the functionality of the codebase by providing a new query option for battles.

--- src/controllers/battlesController.js
+++ src/controllers/battlesController.js
@@ -102,6 +102,23 @@ async function getBattles (req, res) {
     }
 };
 
+ async function getBattlesByVillainName(req, res) {
+    try {
+        const { name } = req.params;
+        const result = await pool.query(
+            `SELECT battles.id, battles.villain1_id, battles.villain2_id, battles.winner_id, battles.loser_id, villains.name AS winner, villains2.name AS loser,villains.level AS winner_level, villains2.level AS loser_level, villains.damage AS winner_damage, villains2.damage AS loser_damage, villains.hp AS winner_hp, villains2.hp AS loser_hp FROM battles INNER JOIN villains ON battles.winner_id = villains.id INNER JOIN villains AS villains2 ON battles.loser_id = villains2.id WHERE villains.name = $1 OR villains2.name = $1`, [name]
+        );
+        if (result.rowCount === 0) {
+            res.status(404).json({ error: 'Battle not found' });
+        } else {
+            res.status(200).json(result.rows[0]);
+        }
+    } catch (error) {
+        console.error("Cannot get this battle", error);
+        res.status(500).json({ error: error.message });
+    }
+};
+
 async function deleteBattle (req, res) {
     const { id } = req.params;
     const query = 'DELETE FROM battles WHERE id = $1';
@@ -119,4 +136,4 @@ async function deleteBattle (req, res) {
     }
 };
 
-module.exports = { BattleWinner, getBattles, deleteBattle };
+module.exports = { BattleWinner, getBattles, getBattlesByVillainName, deleteBattle };
--- src/routes/battlesRoutes.js
+++ src/routes/battlesRoutes.js
@@ -4,6 +4,7 @@ const battlesController = require('../controllers/battlesController');
 
 router.post('/battles', battlesController.BattleWinner);
 router.get('/battles', battlesController.getBattles);
+router.get('/battles/:name', battlesController.getBattlesByVillainName);
 router.delete('/battles/:id', battlesController.deleteBattle);
 
 module.exports = router;
