9024bb171aaa24854274428fefbda6ae7258e37d
arthurborges <arthurborges.senai564@gmail.com>
2024-05-08 15:27:16 -0300

    Refactor database configuration to use environment variables
    The code changes in this commit update the database configuration to use environment variables for the database connection details. This improves security and allows for easier configuration management across different environments. The `.env` file has been added to store the necessary environment variables, and the `dbConfig.js` file has been modified to use these variables. This change ensures that the application can connect to the database using the correct credentials and settings in any environment.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const villainsRoutes = require('./routes/villainsRoutes');
+const battlesRoutes = require('./routes/battlesRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/', villainsRoutes);
+app.use('/', battlesRoutes);
+
+app.listen(PORT, () => {
+    console.log(`☠ Server running on port ${PORT}`);
+});
